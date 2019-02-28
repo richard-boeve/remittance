a8b8b4d8d716cb9e61facb53ad575c797264e37a
Richard-Boeve <richardboeve@hotmail.com>
2019-02-28 19:07:06 +1300

    tests working!

--- test/remittance.js
+++ test/remittance.js
@@ -4,43 +4,64 @@ const truffleAssert = require('truffle-assertions');
 contract('Remittance', (accounts) => {
 
     let remit;
-    let owner = accounts[1]
-    let depositor = accounts[2]
-    let shopAddress = accounts[3]
-    let plainPassword = "0xaaaaaa"
-    let expirePeriod = 600
-    let depositAmount = 100000000000000000
-    let fee = 1000000
-    let gas = 210000
+    let owner = accounts[1];
+    let depositor = accounts[2];
+    let shopAddress = accounts[3];
+    let plainPassword = "0xaaaaaa";
+    let depositAmount = web3.toWei(0.1);
+    let EXPIRE_PERIOD = 600;
+    const FEE = 1000000;
+    const GAS_PRICE = 1000;
 
-    beforeEach("Create a new instance", function () {
-      return Remittance.new({from: owner})
-        .then(function (instance) {
-          remit = instance;
-        });
+    beforeEach("Create a new instance", async () => {
+      remit = await Remittance.new({from: owner, gasPrice: GAS_PRICE})
     });
 
     it("Deposit - verify that a deposit can be made", async () => {
-        const hashedPassword = await remit.generateHashedPassword(shopAddress, plainPassword);
-        const deposit = await remit.deposit(hashedPassword, expirePeriod, {from: depositor, value: depositAmount, gas: 210000})
-        assert.equal(deposit.logs[0].args.sender, depositor, "Fee has not been paid to owner");
-        assert.equal(deposit.logs[1].args.amount, depositAmount, "The deposit wasn't successfull");
+      //Create hashed password
+      const hashedPassword = await remit.generateHashedPassword(shopAddress, plainPassword);  
+      //Check starting balance owner
+      const ownerStartingBalance = await remit.feeBalance(owner);
+      console.log(ownerStartingBalance.toString(10));
+      //Check starting balance shop
+      const shopStartingBalance = (await remit.deposits(hashedPassword))[2];
+      console.log(shopStartingBalance.toString(10));
+      //Submit deposit transaction
+      const depositTxReceipt= await remit.deposit(hashedPassword, EXPIRE_PERIOD, {from: depositor, value: depositAmount});
+      //Checking the transaction event logs
+      assert.equal(depositTxReceipt.logs[0].args.sender, depositor, "Fee has not been paid to owner");
+      assert.strictEqual(depositTxReceipt.logs[1].args.amount.toString(10), depositAmount.toString(10), "The deposit wasn't successfull");
+      //Checking fee balance owner
+      assert.equal(+FEE + +ownerStartingBalance.toString(10), (await remit.feeBalance(owner)).toString(10), "Fee balance is not correct");
+      //Checking balance shop
+      assert.equal(+shopStartingBalance + +depositAmount - +FEE, (await remit.deposits(hashedPassword))[2].toString(10), "Deposit balance is not correct");
     })
 
     it("Deposit - verify that a positive amount must be deposited", async () => {
-        const hashedPassword = await remit.generateHashedPassword(shopAddress, plainPassword);
-        await truffleAssert.fails(remit.deposit(hashedPassword, expirePeriod, {from: depositor, value: 0, gas: 210000})), truffleAssert.ErrorType.REVERT,"You can't deposit 0 or less"
+      //Create hashed password
+      const hashedPassword = await remit.generateHashedPassword(shopAddress, plainPassword);
+      //Verify that a value < fee will result in the transaction being reverted
+      await truffleAssert.fails(remit.deposit(hashedPassword, EXPIRE_PERIOD, {from: depositor, value: 0})), truffleAssert.ErrorType.REVERT,"The deposit needs to be larger than the fee"
     })
 
     it("Withdraw - verify that a withdrawal can be made", async () => {
-      const shop_starting_balance = await web3.eth.getBalance(shopAddress);
-      console.log(shop_starting_balance.valueOf())
+      //Create hashed password
       const hashedPassword = await remit.generateHashedPassword(shopAddress, plainPassword);
-      const deposit = await remit.deposit(hashedPassword, expirePeriod, {from: depositor, value: depositAmount})
-      const Withdraw = await remit.withdrawFunds(plainPassword, {from: shopAddress, gas: 210000})
-      const shop_end_balance = await web3.eth.getBalance(shopAddress);
-      console.log(shop_end_balance.valueOf());
-      assert.equal(shop_end_balance, shop_starting_balance + depositAmount - fee - gas), "Incorrect balance";
+      //Check starting balance shop
+      const shopStartingBalance = await web3.eth.getBalance(shopAddress);
+      //Submit transactions: Deposit and Withdraw
+      const depositTxReceipt = await remit.deposit(hashedPassword, EXPIRE_PERIOD, {from: depositor, value: depositAmount});
+      const withdrawTxReceipt = await remit.withdrawFunds(plainPassword, {from: shopAddress, gasPrice: GAS_PRICE });
+      //Obtain gasUsed from receipt
+      const gasUsed = withdrawTxReceipt.receipt.gasUsed;
+      //Calculate transaction cost
+      const transCost = +gasUsed.toString(10) * +GAS_PRICE.toString(10);
+      //Checking the transaction event logs
+      assert.equal(depositTxReceipt.logs[0].args.sender, depositor, "Fee has not been paid to owner");
+      assert.strictEqual(depositTxReceipt.logs[1].args.amount.toString(10), depositAmount.toString(10), "The deposit wasn't successfull");
+      assert.equal(withdrawTxReceipt.logs[0].args.amount.toString(10), +depositAmount - +FEE, "The withdrawn amount is incorrect" )
+      //Checking the shop balance
+      assert.equal(+shopStartingBalance + +depositAmount - +FEE + -transCost, (await web3.eth.getBalance(shopAddress)).toString(10), "The shop balance is incorrect")
     })
 }) 
   
